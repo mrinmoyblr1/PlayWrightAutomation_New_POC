7b577c895305f57a70b12645047f543fc330c535
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-06-30 17:39:47 -0400

    ASE

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,9 +1,15 @@
 const { test } = require('@playwright/test');
 
-test('First Playwright test', async (browser) => {
-    await
+test('Browser Context Playwright Test', async ({ browser }) => {
+    // Chrome - plugins/cookies
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
 
 
-
-
-})
+test.only('Page Playwright Test', async ({ page }) => {
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    
+});
---
+++ tests/del.spec.js
@@ -0,0 +1,10 @@
+// import { test as base } from '@playwright/test';
+// const test = base.extend({
+//     user: async ({ }, use) => {
+//         const userData = { username: 'john', role: 'admin' };
+//         await use(userData); // makes `user` available in tests
+//     }
+// });
+// test('test with custom user', async ({ user }) => {
+//     console.log(user.username); // 'john'
+// });
