df716bfc0ef6ec1d4537f55902335858bde51ddc
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-01 01:04:28 -0400

    ASD

--- playwright.config.js
+++ playwright.config.js
@@ -3,12 +3,16 @@ import { defineConfig, devices } from '@playwright/test';
 const config = ({
   testDir: './tests',
   timeout: 40 * 1000,
+
   expect: {
     timeout: 40 * 1000,
   },
+  
   reporter: 'html',
   use: {
-    browserName: 'chromium',
+    // browserName: 'chromium',
+    browserName: 'firefox',
+    // browserName: 'webkit',
     headless: false,
     // screenshot: 'on',  //'only-on-failure'
     // //trace: 'retain-on-failure',
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,15 +1,23 @@
-const { test } = require('@playwright/test');
-
+const { test, expect } = require('@playwright/test');
 test('Browser Context Playwright Test', async ({ browser }) => {
     // Chrome - plugins/cookies
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log("The Title is: " + await page.title());
+
+    await page.locator('#username').fill("mrinmoy.blr@gmail.com");
+    await page.locator("[type='password']").fill('Anjali123');
+    await page.locator("#terms").click();
+    await page.locator("#signInBtn").click();
+    await page.pause();
+
+
 });
 
 
-test.only('Page Playwright Test', async ({ page }) => {
+test('Page Playwright Test', async ({ page }) => {
     await page.goto("https://google.com");
-    console.log(await page.title());
-    
+    console.log("The Title is: " + await page.title());
+    await expect(page).toHaveTitle("Google");
 });
