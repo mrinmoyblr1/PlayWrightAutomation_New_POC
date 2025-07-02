c6bd5671ad35947e4ce3c49586cb9ee74202d1f4
mrinmoyblr2 <mrinmoy.blr2@gmail.com>
2025-07-01 23:21:01 -0400

    SDERT

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,16 +5,10 @@ test('Browser Context Playwright Test', async ({ browser }) => {
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log("The Title is: " + await page.title());
-
     await page.locator('#username').fill("mrinmoy.blr@gmail.com");
-
     await page.locator("[type='password']").fill('Anjali123');
-
     await page.locator("#terms").click();
-
     await page.locator("#signInBtn").click();
-
-
     await page.pause();
 
 
