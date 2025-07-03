aab154e7bf8afebcd070ab431abc659564e937c6
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-02 21:47:18 -0400

    AQWE

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -9,17 +9,25 @@ test.only('Browser Context Playwright Test', async ({ browser }) => {
 
     const userName = page.locator('#username');
     const password = page.locator("[type='password']");
+    const signIn = page.locator("#signInBtn");
+
 
     // Chrome - plugins/cookies
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log("The Title is: " + await page.title());
-    await userName.fill("mrinmoy.blr@gmail.com");
+    await userName.fill("mrinmoy.blr1111@gmail.com");
     await password.fill('Anjali123');
     await page.locator("#terms").click();
-    await page.locator("#signInBtn").click();
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+    await userName.fill("");
+    await userName.fill("mrinmoy.blr@gmail.com");
+    await signIn.click();
+
+
 
 
 
