15ed29202dd52f8cc57548b90c2dc6f215c134f9
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-02 22:38:25 -0400

    AQWE

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,39 +1,28 @@
 const { test, expect } = require('@playwright/test');
-
-
 test.only('Browser Context Playwright Test', async ({ browser }) => {
-
     const context = await browser.newContext();
     const page = await context.newPage();
-
-
     const userName = page.locator('#username');
     const password = page.locator("[type='password']");
     const signIn = page.locator("#signInBtn");
-
-
     // Chrome - plugins/cookies
-
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log("The Title is: " + await page.title());
     await userName.fill("mrinmoy.blr1111@gmail.com");
-    await password.fill('Anjali123');
+    await password.fill('Anjali@12');
     await page.locator("#terms").click();
     await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
-
     await userName.fill("");
-    await userName.fill("mrinmoy.blr@gmail.com");
+    await userName.fill("rahulshettyacademy");
+    await password.fill("");
+    await password.fill('learning');
+    await page.locator("#terms").click();
     await signIn.click();
-
-
-
-
-
-
-    //await page.pause();
+    await page.pause();
 });
+
 
 
 test('Page Playwright Test', async ({ page }) => {
