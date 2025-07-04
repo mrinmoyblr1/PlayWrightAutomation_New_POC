3df56448cec291766c85acae0eb3e63eeadb1b2a
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-04 10:02:50 -0400

    From Mac Pro 07-04-2025 001

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+
+test.only('Browser Context Playwright Test111', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/client/");
+
+    await page.locator("#userEmail").fill("mrinmoy.blr@gmail.com");
+    await page.locator("#userPassword").fill("Anjali@12");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+
+
+    // await page.pause();
+});
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,11 @@
 const { test, expect } = require('@playwright/test');
-test.only('Browser Context Playwright Test', async ({ browser }) => {
+test('Browser Context Playwright Test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const userName = page.locator('#username');
     const password = page.locator("[type='password']");
     const signIn = page.locator("#signInBtn");
     const cardTitles = page.locator(".card-body a");
-
     // Chrome - plugins/cookies
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log("The Title is: " + await page.title());
@@ -22,22 +21,13 @@ test.only('Browser Context Playwright Test', async ({ browser }) => {
     await password.fill('learning');
     await page.locator("#terms").click();
     await signIn.click();
-
     // console.log(await cardTitles.nth(1).textContent());
     console.log(await cardTitles.first().textContent());
-
     // Get Names of all the Products
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
-
-
-
-
-
-
     //await page.pause();
 });
-
 
 
 test('Page Playwright Test', async ({ page }) => {
