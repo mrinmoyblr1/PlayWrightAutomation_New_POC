036558b05b7d68f06319c15db25ddf8d4cb74bde
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-04 10:24:57 -0400

    From Mac Pro on 07-04-2025 002

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,18 +1,13 @@
 const { test, expect } = require('@playwright/test');
-
-
 test.only('Browser Context Playwright Test111', async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/client/");
-
     await page.locator("#userEmail").fill("mrinmoy.blr@gmail.com");
     await page.locator("#userPassword").fill("Anjali@12");
     await page.locator("[value='Login']").click();
-    await page.waitForLoadState('networkidle');
-
+    //await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
-
-
     // await page.pause();
 });
