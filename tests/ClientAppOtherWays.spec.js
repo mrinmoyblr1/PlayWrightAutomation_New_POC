e3871205cdc99015ec04717d6302379e96d071ce
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-07-09 02:06:57 -0400

    ASW

--- tests/ClientAppOtherWays.spec.js
+++ tests/ClientAppOtherWays.spec.js
@@ -5,55 +5,19 @@ test.only('Browser Context Playwright Test111', async ({ page }) => {
     const productName = "ZARA COAT 3";
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client/");
-
     await page.getByPlaceholder("email@example.com").fill(email);
     await page.getByPlaceholder("enter your passsword").fill("Anjali@12");
     await page.getByRole("button", { name: 'Login' }).click();
-
     //await page.waitForLoadState('networkidle');
     await page.locator(".card-body b").first().waitFor();
-
     await page.locator(".card-body").filter({ hasText: 'ZARA COAT 3' }).getByRole("button", { name: ' Add To Cart' }).click();
-
-
-
-
-    await page.getByRole("button", { name: '  Cart ' }).click();
-
-
-
+    await page.getByRole("listitem").getByRole("button", { name: '  Cart ' }).click();
     await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    expect(bool).toBeTruthy();
-    await page.locator("text=Checkout").click();
-    await page.locator("[placeholder*='Country']").pressSequentially("Ind");
-    const dropdown = await page.locator(".ta-results");
-    await dropdown.waitFor();
-    const optionsCount = await dropdown.locator("button").count();
-    for (let i = 0; i < optionsCount; i++) {
-        const text = await dropdown.locator("button").nth(i).textContent();
-        if (text === " India") {
-            await dropdown.locator("button").nth(i).click();
-            break;
-        }
-    }
-    expect(await page.locator(".user__name label").first()).toHaveText(email);  // The method toHaveText(email) is part of expect
-    await page.locator(".action__submit").click();
+    expect(await page.getByText("ZARA COAT 3")).toBeVisible();
+    await page.getByRole("button", { name: 'Checkout' }).click();
+    await page.getByPlaceholder("Select Country").pressSequentially("Ind");
+    await page.getByRole("button", { name: "India" }).nth(1).click();
+    await page.getByText("Place Order ").click();
     console.log(await page.locator(".hero-primary").textContent());
-    expect(await page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-    //console.log(await page.locator("label[class=ng-star-inserted]").textContent());
-    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();// Here we used parent css then child css
-    console.log(orderID);
-    await page.locator("button[routerlink*='myorders']").click();
-    await page.locator("tbody").waitFor();
-    const rows = await page.locator("tbody tr");
-    for (let i = 0; i < await rows.count(); i++) {
-        const rowOrderID = await rows.nth(i).locator("th").textContent();
-        if (orderID.includes(rowOrderID)) {
-            await rows.nth(i).locator("button").first().click();
-            break;
-        }
-    }
-    const orderIDDetails = await page.locator(".col-text").textContent();
-    expect(orderID.includes(orderIDDetails)).toBeTruthy();
+    await expect(page.getByText(" Thankyou for the order. ")).toBeVisible();
 });
